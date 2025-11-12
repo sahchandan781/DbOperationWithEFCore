964341f2d244e2610607d92224d2696255fad4a6
Chandan kumar Sah <93442672+sahchandan781@users.noreply.github.com>
2025-11-12 17:12:49 +0530

    created database and  table, inserted master data, did data seeding

--- DbOperationsWithEFCoreApp/DbOperationsWithEFCoreApp/Data/AppDbContext.cs
+++ DbOperationsWithEFCoreApp/DbOperationsWithEFCoreApp/Data/AppDbContext.cs
@@ -6,7 +6,32 @@ namespace DbOperationsWithEFCoreApp.Data
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
-            
+
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Currency>().HasData(
+                new Currency() { Id = 1, Title = "INR", Description = "Indian INR" },
+                new Currency() { Id = 2, Title = "USD", Description = "American DOllar" },
+                new Currency() { Id = 3, Title = "Euro", Description = "Euro" },
+                new Currency() { Id = 4, Title = "Dinar", Description = "Dollar" }
+                );
+
+            modelBuilder.Entity<Language>().HasData(
+                new Language() { LanguageId = 1, Title = "Hindi", Description = "Hindi" },
+                new Language() { LanguageId = 2, Title = "Tamil", Description = "Tamil" },
+                new Language() { LanguageId = 3, Title = "Punjabi", Description = "Punjabi" },
+                new Language() { LanguageId = 4, Title = "English", Description = "English" }
+                );
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Language> Languages { get; set; }
+        public DbSet<Currency> Currencies { get; set; }
+        public DbSet<BookPrice> BookPrices { get; set; }
+
+
+
     }
 }
---
+++ DbOperationsWithEFCoreApp/DbOperationsWithEFCoreApp/Data/Language.cs
@@ -0,0 +1,15 @@
+﻿namespace DbOperationsWithEFCoreApp.Data
+{
+    public class Language
+    {
+        public int LanguageId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public ICollection<Book> Books { get; set;  }
+
+
+    }
+}
